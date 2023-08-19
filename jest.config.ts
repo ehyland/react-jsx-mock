09bd44d44635fc0568060d9e12146226e8d184b2
Eamon Hyland <ehyland90@gmail.com>
2023-08-19 10:58:47 +1000

    clean-up test config

--- jest.config.ts
+++ jest.config.ts
@@ -11,22 +11,6 @@ const config: Config = {
   testEnvironment: '@happy-dom/jest-environment',
   testMatch: ['**/packages/**/?(*.)+(spec|test).[jt]s?(x)'],
   snapshotSerializers: ['jest-snapshot-serializer-ansi'],
-  // testMatch: ['./packages/**/test.[jt]s?(x)'],
-
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
-
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-  // snapshotSerializers: [],
-
-  // A map from regular expressions to paths to transformers
-  // transform: undefined,
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
 };
 
 export default config;
